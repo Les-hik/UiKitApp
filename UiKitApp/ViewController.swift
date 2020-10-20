b9e613b92c9cfb82d3d2f2e2c1226484be998ad0
Alex <ale-x@list.ru>
2020-10-20 20:15:51 +0300

    ver 1.0.0

--- UiKitApp/ViewController.swift
+++ UiKitApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textLable: UILabel!
+    @IBOutlet var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLable.text = "text"
+        textLable.font = UIFont.boldSystemFont(ofSize: 35)
+        textLable.textAlignment = .center
+        textLable.numberOfLines = 4
+        segmentControl.insertSegment(withTitle: "Third", at: 2, animated: false)
     }
-
-
+    
+    @IBAction func segmentedControlAcnion() {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            textLable.text = "Нажата кнопка 1"
+            textLable.textColor = .red
+        case 1:
+            textLable.text = "Нажата кнопка 2"
+            textLable.textColor = .green
+        case 2:
+            textLable.text = "Нажата кнопка 3"
+            textLable.textColor = .orange
+            textLable.font = UIFont.italicSystemFont(ofSize: 40)
+        default: break
+        }
+    }
 }
 
